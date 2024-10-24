2f5a90afaf733aaace65cbf73d9c832180e6a658
Tyler <tyler.hagstrom@gmail.com>
2024-10-24 18:53:05 -0500

    working lsp config

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -178,6 +178,7 @@ return {
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
+        omnisharp = {},
         --
 
         lua_ls = {
@@ -209,6 +210,8 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'omnisharp',
+        'csharpier',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
